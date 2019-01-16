52d07680e189e9be3a67e4d91823f41fde19d70b
架构-李铁拴 <litieshuan@fclassroom.com>
2019-01-16 10:14:49 +0800

    bug:feign date format

--- common-feign/src/main/java/org/corefine/common/feign/ClassPathClientScanner.java
+++ common-feign/src/main/java/org/corefine/common/feign/ClassPathClientScanner.java
@@ -1,8 +1,6 @@
 package org.corefine.common.feign;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,50 +9,29 @@ import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.BeanDefinitionHolder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.context.annotation.ClassPathBeanDefinitionScanner;
-import org.springframework.core.env.Environment;
 import org.springframework.core.type.filter.AnnotationTypeFilter;
 
-import java.text.SimpleDateFormat;
 import java.util.Set;
-import java.util.TimeZone;
 
 public class ClassPathClientScanner extends ClassPathBeanDefinitionScanner {
     private static final Logger LOGGER = LoggerFactory.getLogger(ClassPathClientScanner.class);
-    private final static String DATE_FORMAT_NAME = "spring.jackson.date-format";
-    private final static String TIME_ZONE_NAME = "spring.jackson.time-zone";
     private ObjectMapper objectMapper;
 
-
-    public ClassPathClientScanner(BeanDefinitionRegistry registry) {
+    public ClassPathClientScanner(BeanDefinitionRegistry registry, ObjectMapper objectMapper) {
         super(registry, false);
-    }
-
-    public void registerFilters() {
-        addIncludeFilter(new AnnotationTypeFilter(FeignClient.class));
-        addExcludeFilter((metadataReader, metadataReaderFactory) -> {
-            String className = metadataReader.getClassMetadata().getClassName();
-            return className.endsWith("package-info");
-        });
+        this.objectMapper = objectMapper;
     }
 
     @Override
     public Set<BeanDefinitionHolder> doScan(String... basePackages) {
         Set<BeanDefinitionHolder> beanDefinitions = super.doScan(basePackages);
-        this.initObjectMapper();
         processBeanDefinitions(beanDefinitions);
         return beanDefinitions;
     }
 
-    private void initObjectMapper() {
-        objectMapper = new ObjectMapper();
-        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL).configure(SerializationFeature.INDENT_OUTPUT, true);
-        Environment environment = getEnvironment();
-        if (environment.containsProperty(TIME_ZONE_NAME)) {
-            objectMapper.setTimeZone(TimeZone.getTimeZone(environment.getProperty(TIME_ZONE_NAME)));
-        }
-        if (environment.containsProperty(DATE_FORMAT_NAME)) {
-            objectMapper.setDateFormat(new SimpleDateFormat(environment.getProperty(DATE_FORMAT_NAME)));
-        }
+    public void registerFilters() {
+        addIncludeFilter(new AnnotationTypeFilter(FeignClient.class));
+        addExcludeFilter((metadataReader, metadataReaderFactory) -> metadataReader.getClassMetadata().getClassName().endsWith("package-info"));
     }
 
     private void processBeanDefinitions(Set<BeanDefinitionHolder> beanDefinitions) {
@@ -62,7 +39,7 @@ public class ClassPathClientScanner extends ClassPathBeanDefinitionScanner {
         for (BeanDefinitionHolder holder : beanDefinitions) {
             definition = (AnnotatedBeanDefinition) holder.getBeanDefinition();
             String beanClassName = definition.getBeanClassName();
-            definition.getConstructorArgumentValues().addGenericArgumentValue(beanClassName); // issue #59
+            definition.getConstructorArgumentValues().addGenericArgumentValue(beanClassName);
             definition.setBeanClassName(FeignClientFactoryBean.class.getName());
             String name = (String) definition.getMetadata().getAnnotationAttributes(FeignClient.class.getCanonicalName()).get("value");
             definition.getPropertyValues().add("name", name);
--- common-feign/src/main/java/org/corefine/common/feign/FeignClientRegistrar.java
+++ common-feign/src/main/java/org/corefine/common/feign/FeignClientRegistrar.java
@@ -1,5 +1,10 @@
 package org.corefine.common.feign;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeansException;
@@ -7,23 +12,49 @@ import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.BeanFactoryAware;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
 import org.springframework.boot.autoconfigure.AutoConfigurationPackages;
+import org.springframework.context.EnvironmentAware;
 import org.springframework.context.ResourceLoaderAware;
 import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.core.env.Environment;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.type.AnnotationMetadata;
 import org.springframework.util.StringUtils;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.TimeZone;
 
-public class FeignClientRegistrar implements ImportBeanDefinitionRegistrar, BeanFactoryAware, ResourceLoaderAware {
+public class FeignClientRegistrar implements ImportBeanDefinitionRegistrar, BeanFactoryAware, ResourceLoaderAware, EnvironmentAware {
     private final static Logger logger = LoggerFactory.getLogger(FeignClientRegistrar.class);
+    private final static String DATE_FORMAT_NAME = "spring.jackson.date-format";
+    private final static String TIME_ZONE_NAME = "spring.jackson.time-zone";
     private ResourceLoader resourceLoader;
     private BeanFactory beanFactory;
+    private ObjectMapper objectMapper;
+
+    private void initObjectMapper(Environment environment) {
+        objectMapper = new ObjectMapper();
+        objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+        objectMapper.configure(SerializationFeature.INDENT_OUTPUT, true);
+        if (environment.containsProperty(TIME_ZONE_NAME)) {
+            String timeZone = environment.getProperty(TIME_ZONE_NAME);
+            objectMapper.setTimeZone(TimeZone.getTimeZone(timeZone));
+            logger.info("jackson time zone " + timeZone);
+        }
+        if (environment.containsProperty(DATE_FORMAT_NAME)) {
+            String dateFormat = environment.getProperty(DATE_FORMAT_NAME);
+            if (dateFormat != null) {
+                objectMapper.setDateFormat(new SimpleDateFormat(dateFormat));
+                logger.info("jackson date format " + dateFormat);
+            }
+        }
+    }
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata metadata, BeanDefinitionRegistry registry) {
         logger.debug("Searching @FeignClient");
-        ClassPathClientScanner scanner = new ClassPathClientScanner(registry);
+        ClassPathClientScanner scanner = new ClassPathClientScanner(registry, objectMapper);
         try {
             if (this.resourceLoader != null) {
                 scanner.setResourceLoader(this.resourceLoader);
@@ -48,5 +79,10 @@ public class FeignClientRegistrar implements ImportBeanDefinitionRegistrar, Bean
     @Override
     public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
         this.beanFactory = beanFactory;
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.initObjectMapper(environment);
     }
 }
