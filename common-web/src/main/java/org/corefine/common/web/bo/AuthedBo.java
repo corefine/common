97b5f345e7c50e78d400f70c65f62445864e7c66
架构-李铁拴 <litieshuan@fclassroom.com>
2019-06-17 11:11:23 +0800

    web:增加user bo的支持

--- common-web/src/main/java/org/corefine/common/web/bo/AuthedBo.java
+++ common-web/src/main/java/org/corefine/common/web/bo/AuthedBo.java
@@ -1,15 +1,3 @@
 package org.corefine.common.web.bo;
 
-public class AuthedBo implements Authed {
-    private Long userId;
-
-    @Override
-    public void setUserId(Long userId) {
-        this.userId = userId;
-
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-}
+public class AuthedBo extends UserBo implements Authed {}
---
+++ common-web/src/main/java/org/corefine/common/web/bo/UserBo.java
@@ -0,0 +1,14 @@
+package org.corefine.common.web.bo;
+
+public class UserBo {
+    private Long userId;
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+}
